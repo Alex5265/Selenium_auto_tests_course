edbf1a4e4cb65b289d8221799d307bd4cdf5000d
Alex5265 <deadtaras@mail.ru>
2024-01-09 21:31:49 +0300

    added new task of new themes - TUPLE

---
+++ generation_python_advansed/tuple/tuple_tusk_01.py
@@ -0,0 +1,7 @@
+# Тренировка работы с кортежами.
+# Задание - необходимо дополнить код используя индексацию, чтобы переменная ласт содержала
+# последний элемент кортежа
+
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+last = countries[-1]
+print(last)
