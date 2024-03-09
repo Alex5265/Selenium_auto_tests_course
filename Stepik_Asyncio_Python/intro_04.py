afa32ab0f97d01efa3cdb058f99cdf6ab40816fd
Alex5265 <deadtaras@mail.ru>
2024-03-09 22:06:36 +0300

    ADDED first intro task. teacher just show strings kode. dont say how it works

---
+++ Stepik_Asyncio_Python/intro_04.py
@@ -0,0 +1,20 @@
+import asyncio
+
+async def fetch_data():
+    print("Загрузка данных...")
+    await asyncio.sleep(2)
+    return "Данные загружены"
+
+async def process_data():
+    print("Обработка данных...")
+    await asyncio.sleep(1)
+    return "Данные обработаны"
+
+async def main():
+    fetched_data = await fetch_data()
+    processed_data = await process_data()
+    print(fetched_data, processed_data)
+
+asyncio.run(main())
+
+
